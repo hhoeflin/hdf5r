d99628174fab793739dfcdd18c74761f3016db7d
Holger Hoefling <hhoeflin@gmail.com>
2017-09-06 23:43:31 +0200

    Also add attributes and subgroups to file test

--- tests/testthat/test-print.R
+++ tests/testthat/test-print.R
@@ -10,14 +10,23 @@ test_that("Print functions work as expected", {
 
     ## open a new one, truncate if it exists
     file.h5 <- H5File$new(test_file, mode="w")
+    h5group <- file.h5$create_group("test")
 
+    ## add some attributes
+    h5attr(file.h5, "attr1") <- 1.0
+    h5attr(file.h5, "attr2") <- "A"
+    h5attr(h5group, "grp_attr") <- "a group"
+    
     ## H5File has its location as part of its output
     expect_output(file.h5$print(), regexp=paste0("Class: H5File\nFilename: ",
                                                  escape_regexp(normalizePath(test_file, mustWork=FALSE)),
-                                                 "\nAccess type: H5F_ACC_RDWR"))
+                                                 "\nAccess type: H5F_ACC_RDWR\n",
+                                                 "Attributes: attr1, attr2\n",
+                                                 "Listing:\n",
+                                                 " name    object.type dataset.dims dataset.type_class\n",
+                                                 " test H5O_TYPE_GROUP         <NA>               <NA>"))
 
     #H5Group also prints the name of the group
-    h5group <- file.h5$create_group("test")
     expect_output(h5group$print(), regexp=paste0("Class: H5Group\nID: 0x[0-9A-F]+\nFilename: ",
                                                  escape_regexp(normalizePath(test_file, mustWork=FALSE)),
                                                  "\nGroup: /test"))
