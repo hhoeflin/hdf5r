1e2c8a2497766966c34e580968b697ac23af249f
Holger Hoefling <hhoeflin@gmail.com>
2020-01-03 19:55:37 +0000

    reduce part of log for hdf5 installation

--- travis_setup.sh
+++ travis_setup.sh
@@ -10,6 +10,6 @@ else # install from source
   tar -xzf "hdf5-$HDF5_VERSION.tar.gz"
   cd "hdf5-$HDF5_VERSION"
   ./configure --prefix=/usr/local
-  sudo make install | tail -n1000
+  sudo make install | tail -n100
   cd ../hdf5r
 fi
