1b9e7bf16f488a1c5aa16cf2a640ddf44bcf6a91
Holger Hoefling <hhoeflin@gmail.com>
2020-01-03 20:27:19 +0000

    dumping stderr log of hdf5-install to /dev/null

--- travis_setup.sh
+++ travis_setup.sh
@@ -10,6 +10,6 @@ else # install from source
   tar -xzf "hdf5-$HDF5_VERSION.tar.gz"
   cd "hdf5-$HDF5_VERSION"
   ./configure --prefix=/usr/local
-  sudo make install > /dev/null
+  sudo make install > /dev/null 2> /dev/null
   cd ../hdf5r
 fi
